cc2392417873c0872052b683ca1af01add7320c4
liuchang <liuchang@fit2cloud.com>
2023-02-15 17:10:43 +0800

    refactor(优化代码): 限制只有当配置dev环境的时候才注入

--- src/main/java/run/halo/app/just/auth/config/JustAuthWebSecurityConfig.java
+++ src/main/java/run/halo/app/just/auth/config/JustAuthWebSecurityConfig.java
@@ -1,6 +1,7 @@
 package run.halo.app.just.auth.config;
 
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -49,6 +50,9 @@ public class JustAuthWebSecurityConfig {
     }
 
     @Bean
+    @ConditionalOnProperty(name = "spring.profiles.active",
+        havingValue = "dev",
+        matchIfMissing = false)
     AuthSettingInit authSettingInit(ReactiveExtensionClient client) {
         return new AuthSettingInit(client);
     }
